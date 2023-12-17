b50d8e214a4948ecbd84981c431c7084f79038fc
Altun98 <92326419+Altun98@users.noreply.github.com>
2023-12-17 21:56:23 +0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] { "Engin", "Murad", "Hesen", "Abbas", "Merdan", "Tural", "Elnur", "Xaliq" };
+            foreach (var item in isimler)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("******************");
+            List<string> isimler2 = new List<string> { "Engin", "Murad", "Hesen", "Abbas", "Merdan", "Tural", "Elnur", "Xaliq","Zakir","Uqur"};
+            isimler2.Add("SAMIR");
+            isimler2.Add("XXX");
+            foreach (var item in isimler2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+           
+            isimler.Add("Hesen");
+            foreach (var VARIABLE in isimler)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
